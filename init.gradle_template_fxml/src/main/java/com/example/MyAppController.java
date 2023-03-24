d16602873d45c484d795a74cd537e99e174d27bf
sosuisen <hidekazu.kubota@gmail.com>
2023-03-24 14:16:19 +0900

    fix: add import

--- init.gradle_template_fxml/src/main/java/com/example/MyAppController.java
+++ init.gradle_template_fxml/src/main/java/com/example/MyAppController.java
@@ -1,6 +1,7 @@
 package com.example;
 
 import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
