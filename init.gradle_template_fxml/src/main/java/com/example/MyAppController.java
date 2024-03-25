a3143b9c7ad8d54548eda84890c64a3ac113dfe6
sosuisen <hidekazu.kubota@gmail.com>
2024-03-25 22:28:57 +0900

    fix: MyAppController

--- init.gradle_template_fxml/src/main/java/com/example/MyAppController.java
+++ init.gradle_template_fxml/src/main/java/com/example/MyAppController.java
@@ -22,7 +22,9 @@ public class MyAppController {
 	public void initialize() {
 		var javaVersion = System.getProperty("java.version");
 		var javafxVersion = System.getProperty("javafx.version");
-
+		
 		label.setText("Hello, JavaFX " + javafxVersion + "\nRunning on Java " + javaVersion + ".");
+
+		button.setOnAction(e -> Platform.exit());
 	}
 }
